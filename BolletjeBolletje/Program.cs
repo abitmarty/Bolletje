9fd529f1bd3e7c237c91f896604c351b161362ba
abitmarty <martijn.totte@Gmail.com>
2020-10-06 13:44:33 +0200

    Added settings form and settings button

--- BolletjeBolletje/Program.cs
+++ BolletjeBolletje/Program.cs
@@ -1,22 +1,139 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Drawing;
 using System.Windows.Forms;
 
-namespace BolletjeBolletje
+class ReversiForm : Form
 {
-    static class Program
+    private Button buttonHelp;
+    private Label labelTurn;
+    private Label labelP1Name;
+    private Label labelP1Points;
+    private Label labelP2Points;
+    private Label labelP2Name;
+    private Button buttonSettings;
+    private Button buttonNewGame;
+
+    public ReversiForm()
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
+        // Set at top to initialise components
+        this.InitializeComponent();
+        this.buttonSettings.Click += this.openSettings;
+
+        
+    }
+
+    static void Main()
+    {
+        ReversiForm screen = new ReversiForm();
+        Application.Run(screen);
+    }
+
+    public void openSettings(object obj, EventArgs pea)
+    {
+        Settingsform screen = new Settingsform();
+        screen.Show();
+        Console.WriteLine("Settings");
+    }
+
+    private void InitializeComponent()
+    {
+            this.buttonNewGame = new System.Windows.Forms.Button();
+            this.buttonHelp = new System.Windows.Forms.Button();
+            this.labelTurn = new System.Windows.Forms.Label();
+            this.labelP1Name = new System.Windows.Forms.Label();
+            this.labelP1Points = new System.Windows.Forms.Label();
+            this.labelP2Points = new System.Windows.Forms.Label();
+            this.labelP2Name = new System.Windows.Forms.Label();
+            this.buttonSettings = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonNewGame
+            // 
+            this.buttonNewGame.Location = new System.Drawing.Point(24, 22);
+            this.buttonNewGame.Name = "buttonNewGame";
+            this.buttonNewGame.Size = new System.Drawing.Size(75, 23);
+            this.buttonNewGame.TabIndex = 0;
+            this.buttonNewGame.Text = "New game";
+            this.buttonNewGame.UseVisualStyleBackColor = true;
+            // 
+            // buttonHelp
+            // 
+            this.buttonHelp.Location = new System.Drawing.Point(118, 22);
+            this.buttonHelp.Name = "buttonHelp";
+            this.buttonHelp.Size = new System.Drawing.Size(75, 23);
+            this.buttonHelp.TabIndex = 1;
+            this.buttonHelp.Text = "Help";
+            this.buttonHelp.UseVisualStyleBackColor = true;
+            // 
+            // labelTurn
+            // 
+            this.labelTurn.AutoSize = true;
+            this.labelTurn.Location = new System.Drawing.Point(21, 60);
+            this.labelTurn.Name = "labelTurn";
+            this.labelTurn.Size = new System.Drawing.Size(29, 13);
+            this.labelTurn.TabIndex = 2;
+            this.labelTurn.Text = "Turn";
+            // 
+            // labelP1Name
+            // 
+            this.labelP1Name.AutoSize = true;
+            this.labelP1Name.Location = new System.Drawing.Point(21, 91);
+            this.labelP1Name.Name = "labelP1Name";
+            this.labelP1Name.Size = new System.Drawing.Size(49, 13);
+            this.labelP1Name.TabIndex = 3;
+            this.labelP1Name.Text = "P1 name";
+            // 
+            // labelP1Points
+            // 
+            this.labelP1Points.AutoSize = true;
+            this.labelP1Points.Location = new System.Drawing.Point(21, 114);
+            this.labelP1Points.Name = "labelP1Points";
+            this.labelP1Points.Size = new System.Drawing.Size(51, 13);
+            this.labelP1Points.TabIndex = 4;
+            this.labelP1Points.Text = "P1 points";
+            // 
+            // labelP2Points
+            // 
+            this.labelP2Points.AutoSize = true;
+            this.labelP2Points.Location = new System.Drawing.Point(115, 114);
+            this.labelP2Points.Name = "labelP2Points";
+            this.labelP2Points.Size = new System.Drawing.Size(49, 13);
+            this.labelP2Points.TabIndex = 6;
+            this.labelP2Points.Text = "P2 name";
+            // 
+            // labelP2Name
+            // 
+            this.labelP2Name.AutoSize = true;
+            this.labelP2Name.Location = new System.Drawing.Point(115, 91);
+            this.labelP2Name.Name = "labelP2Name";
+            this.labelP2Name.Size = new System.Drawing.Size(51, 13);
+            this.labelP2Name.TabIndex = 5;
+            this.labelP2Name.Text = "P2 points";
+            // 
+            // buttonSettings
+            // 
+            this.buttonSettings.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.buttonSettings.Location = new System.Drawing.Point(344, 22);
+            this.buttonSettings.Name = "buttonSettings";
+            this.buttonSettings.Size = new System.Drawing.Size(28, 23);
+            this.buttonSettings.TabIndex = 7;
+            this.buttonSettings.Text = "⚙";
+            this.buttonSettings.UseVisualStyleBackColor = true;
+            // 
+            // ReversiForm
+            // 
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.buttonSettings);
+            this.Controls.Add(this.labelP2Points);
+            this.Controls.Add(this.labelP2Name);
+            this.Controls.Add(this.labelP1Points);
+            this.Controls.Add(this.labelP1Name);
+            this.Controls.Add(this.labelTurn);
+            this.Controls.Add(this.buttonHelp);
+            this.Controls.Add(this.buttonNewGame);
+            this.Name = "ReversiForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
     }
 }
---
+++ BolletjeBolletje/Settings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+class Settingsform : Form
+{
+
+    public Settingsform()
+    {
+        // Set at top to initialise components
+        this.InitializeComponent();
+    }
+
+    public void openSettings(object obj, EventArgs pea)
+    {
+        ReversiForm screen = new ReversiForm();
+        Application.Run(screen);
+        Console.WriteLine("Settings");
+    }
+
+    private void InitializeComponent()
+    {
+            this.SuspendLayout();
+            // 
+            // Settingsform
+            // 
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Name = "Settingsform";
+            this.ResumeLayout(false);
+
+    }
+}
