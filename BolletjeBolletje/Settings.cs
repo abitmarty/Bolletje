64801911ac1898914cea06b90aeddc18094e4ad5
abitmarty <matijn.totte@gmail.com>
2020-10-19 17:49:15 +0200

    Made change tile size a setting

--- BolletjeBolletje/Settings.cs
+++ BolletjeBolletje/Settings.cs
@@ -18,6 +18,10 @@ class Settingsform : Form
     private ComboBox comboBox1;
     private Label labelPlayer1;
     private ComboBox comboBox2;
+    private Label labelXTileWidth;
+    private Label labelYTileWidth;
+    private TextBox textBoxTileWidth;
+    private TextBox textBoxTileHeight;
 
     // Variables
     private SettingsInitials currentSettings;
@@ -53,10 +57,6 @@ class Settingsform : Form
         this.setTextboxes();
 
         this.buttonSave.Click += this.saveSettings;
-
-        // Standard icon is Man and Fish
-        this.comboBox1.Text = this.currentSettings.getP1Icon();
-        this.comboBox2.Text = this.currentSettings.getP2Icon();
     }
 
     private void drawingSettingsLine(object obj, PaintEventArgs pea)
@@ -91,6 +91,9 @@ class Settingsform : Form
         this.currentSettings.setTilesX(Convert.ToInt32(this.textBoxXTiles.Text) - 1);
         this.currentSettings.setTilesY(Convert.ToInt32(this.textBoxYTiles.Text) - 1);
 
+        this.currentSettings.setTileSizeX(Convert.ToInt32(this.textBoxTileWidth.Text));
+        this.currentSettings.setTileSizeY(Convert.ToInt32(this.textBoxTileHeight.Text));
+
         this.Close();
     }
 
@@ -106,6 +109,14 @@ class Settingsform : Form
         // Do plus one to be user friendly
         this.textBoxXTiles.Text = (this.currentSettings.getTilesX() + 1).ToString();
         this.textBoxYTiles.Text = (this.currentSettings.getTilesY() + 1).ToString();
+
+        // Standard icon is Man and Fish
+        this.comboBox1.Text = this.currentSettings.getP1Icon();
+        this.comboBox2.Text = this.currentSettings.getP2Icon();
+
+        // Tile sizes
+        this.textBoxTileWidth.Text = this.currentSettings.getTileSizeX().ToString();
+        this.textBoxTileHeight.Text = this.currentSettings.getTileSizeY().ToString();
     }
 
     public void openSettings(object obj, EventArgs pea)
@@ -127,20 +138,24 @@ class Settingsform : Form
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.labelPlayer1 = new System.Windows.Forms.Label();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.labelXTileWidth = new System.Windows.Forms.Label();
+            this.labelYTileWidth = new System.Windows.Forms.Label();
+            this.textBoxTileWidth = new System.Windows.Forms.TextBox();
+            this.textBoxTileHeight = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBoxPlayer1
             // 
             this.textBoxPlayer1.Location = new System.Drawing.Point(11, 43);
             this.textBoxPlayer1.Name = "textBoxPlayer1";
-            this.textBoxPlayer1.Size = new System.Drawing.Size(100, 22);
+            this.textBoxPlayer1.Size = new System.Drawing.Size(100, 20);
             this.textBoxPlayer1.TabIndex = 0;
             // 
             // textBoxPlayer2
             // 
             this.textBoxPlayer2.Location = new System.Drawing.Point(147, 43);
             this.textBoxPlayer2.Name = "textBoxPlayer2";
-            this.textBoxPlayer2.Size = new System.Drawing.Size(100, 22);
+            this.textBoxPlayer2.Size = new System.Drawing.Size(100, 20);
             this.textBoxPlayer2.TabIndex = 1;
             // 
             // labelPlayer2
@@ -150,7 +165,7 @@ class Settingsform : Form
             this.labelPlayer2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(107)))), ((int)(((byte)(107)))));
             this.labelPlayer2.Location = new System.Drawing.Point(147, 15);
             this.labelPlayer2.Name = "labelPlayer2";
-            this.labelPlayer2.Size = new System.Drawing.Size(69, 18);
+            this.labelPlayer2.Size = new System.Drawing.Size(59, 15);
             this.labelPlayer2.TabIndex = 3;
             this.labelPlayer2.Text = "Player 2";
             this.labelPlayer2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -175,7 +190,7 @@ class Settingsform : Form
             this.labelXTiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelXTiles.Location = new System.Drawing.Point(8, 133);
             this.labelXTiles.Name = "labelXTiles";
-            this.labelXTiles.Size = new System.Drawing.Size(122, 18);
+            this.labelXTiles.Size = new System.Drawing.Size(104, 15);
             this.labelXTiles.TabIndex = 8;
             this.labelXTiles.Text = "Horizontal tiles";
             // 
@@ -185,7 +200,7 @@ class Settingsform : Form
             this.labelYTiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelYTiles.Location = new System.Drawing.Point(144, 133);
             this.labelYTiles.Name = "labelYTiles";
-            this.labelYTiles.Size = new System.Drawing.Size(100, 18);
+            this.labelYTiles.Size = new System.Drawing.Size(86, 15);
             this.labelYTiles.TabIndex = 7;
             this.labelYTiles.Text = "Vertical tiles";
             // 
@@ -193,14 +208,14 @@ class Settingsform : Form
             // 
             this.textBoxXTiles.Location = new System.Drawing.Point(11, 153);
             this.textBoxXTiles.Name = "textBoxXTiles";
-            this.textBoxXTiles.Size = new System.Drawing.Size(100, 22);
+            this.textBoxXTiles.Size = new System.Drawing.Size(100, 20);
             this.textBoxXTiles.TabIndex = 6;
             // 
             // textBoxYTiles
             // 
             this.textBoxYTiles.Location = new System.Drawing.Point(143, 153);
             this.textBoxYTiles.Name = "textBoxYTiles";
-            this.textBoxYTiles.Size = new System.Drawing.Size(100, 22);
+            this.textBoxYTiles.Size = new System.Drawing.Size(100, 20);
             this.textBoxYTiles.TabIndex = 5;
             // 
             // comboBox1
@@ -208,7 +223,7 @@ class Settingsform : Form
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(11, 71);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(100, 24);
+            this.comboBox1.Size = new System.Drawing.Size(100, 21);
             this.comboBox1.TabIndex = 9;
             this.comboBox1.SelectionChangeCommitted += new System.EventHandler(this.comboBox1GetString);
             // 
@@ -219,7 +234,7 @@ class Settingsform : Form
             this.labelPlayer1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(107)))), ((int)(((byte)(200)))), ((int)(((byte)(252)))));
             this.labelPlayer1.Location = new System.Drawing.Point(11, 15);
             this.labelPlayer1.Name = "labelPlayer1";
-            this.labelPlayer1.Size = new System.Drawing.Size(69, 18);
+            this.labelPlayer1.Size = new System.Drawing.Size(59, 15);
             this.labelPlayer1.TabIndex = 10;
             this.labelPlayer1.Text = "Player 1";
             this.labelPlayer1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -229,14 +244,52 @@ class Settingsform : Form
             this.comboBox2.FormattingEnabled = true;
             this.comboBox2.Location = new System.Drawing.Point(147, 71);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(100, 24);
+            this.comboBox2.Size = new System.Drawing.Size(100, 21);
             this.comboBox2.TabIndex = 11;
             this.comboBox2.SelectionChangeCommitted += new System.EventHandler(this.comboBox2GetString);
+            // 
+            // labelXTileWidth
+            // 
+            this.labelXTileWidth.AutoSize = true;
+            this.labelXTileWidth.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelXTileWidth.Location = new System.Drawing.Point(8, 195);
+            this.labelXTileWidth.Name = "labelXTileWidth";
+            this.labelXTileWidth.Size = new System.Drawing.Size(104, 15);
+            this.labelXTileWidth.TabIndex = 15;
+            this.labelXTileWidth.Text = "Tile width";
+            // 
+            // labelYTileWidth
+            // 
+            this.labelYTileWidth.AutoSize = true;
+            this.labelYTileWidth.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelYTileWidth.Location = new System.Drawing.Point(144, 195);
+            this.labelYTileWidth.Name = "labelYTileWidth";
+            this.labelYTileWidth.Size = new System.Drawing.Size(86, 15);
+            this.labelYTileWidth.TabIndex = 14;
+            this.labelYTileWidth.Text = "Tile height";
+            // 
+            // textBoxTileWidth
+            // 
+            this.textBoxTileWidth.Location = new System.Drawing.Point(11, 215);
+            this.textBoxTileWidth.Name = "textBoxTileWidth";
+            this.textBoxTileWidth.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTileWidth.TabIndex = 13;
+            // 
+            // textBoxTileHeight
+            // 
+            this.textBoxTileHeight.Location = new System.Drawing.Point(143, 215);
+            this.textBoxTileHeight.Name = "textBoxTileHeight";
+            this.textBoxTileHeight.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTileHeight.TabIndex = 12;
             // 
             // Settingsform
             // 
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(384, 193);
+            this.ClientSize = new System.Drawing.Size(384, 263);
+            this.Controls.Add(this.labelXTileWidth);
+            this.Controls.Add(this.labelYTileWidth);
+            this.Controls.Add(this.textBoxTileWidth);
+            this.Controls.Add(this.textBoxTileHeight);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.labelPlayer1);
             this.Controls.Add(this.comboBox1);
